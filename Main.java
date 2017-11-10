9db34bbf865dc0b061248c2154573d7c989cfb20
svensay <svensay@gmail.com>
2017-11-10 20:18:33 +0100

    Ajout des fonction demander dans l'exo4 et toujours probleme de unchecked

--- Main.java
+++ Main.java
@@ -4,11 +4,45 @@ import java.util.*;
 
 public class Main {
 
+    public static void carree(Queue<Integer> q) {
+        long debut = System.currentTimeMillis();
+        Integer x = 0;
+        for (int i = 0; i < q.size(); i++) {
+            x = q.poll();
+            q.offer(x*x);
+        }
+        //affiche la durée d'exéctuion en milliseconds
+        System.out.println(System.currentTimeMillis() - debut +" ms");
+    }
+
+    public static int addition(Queue<Integer> q) {
+        long debut = System.currentTimeMillis();
+        Integer res = 0;
+        for (int i = 0; i < q.size(); i++) {
+            res += q.poll();
+        }
+        //affiche la durée d'exéctuion en milliseconds
+        System.out.println(System.currentTimeMillis() - debut + " ms");
+        return res;
+    }
+
+    public static boolean haveFive(Queue<Integer> q) {
+        long debut = System.currentTimeMillis();
+        for (int i = 0; i < q.size(); i++) {
+            if (q.peek().compareTo(5) == 0) {
+                System.out.println(System.currentTimeMillis() - debut);
+                return true;
+            }
+        }
+        System.out.println(System.currentTimeMillis() - debut+ " ms");
+        return false;
+    }
+
     public static void main(String[] args) {
         //TEST AVEC UN TAS D'INTEGER
         TBQueue<Integer> t = new TBQueue<Integer>(new Comparator<Integer>() {
             public int compare(Integer x, Integer y) {
-               return x.compareTo(y);
+                return x.compareTo(y);
             }
         }, 10);
         System.out.println(t.offer(2));
@@ -41,20 +75,22 @@ public class Main {
             System.out.print(x + " ");
         }
         System.out.println();
-        
+
         //TEST DE LA METHODE "MAP"
-        QueueExt<Integer> map = t.map((Integer x) -> {return x+5;});
+        QueueExt<Integer> map = t.map((Integer x) -> {
+            return x + 5;
+        });
         System.out.print("Map : ");
         for (Object x : map) {
             System.out.print(x + " ");
         }
         System.out.println();
         //TEST DE LA METHODE "TROUVE"
-        System.out.println("Trouve : "+t.trouve((Integer x) -> x <= 4));
-        
+        System.out.println("Trouve : " + t.trouve((Integer x) -> x <= 4));
+
         //TEST DE LA METHODE "REDUIT"
-        System.out.println("Reduit : "+t.reduit(0, (Integer x,Integer y) -> x+y));
-        
+        System.out.println("Reduit : " + t.reduit(0, (Integer x, Integer y) -> x + y));
+
         // TEST AVEC UN TAS DE STRING
         TBQueue<String> g = new TBQueue<String>(new Comparator<String>() {
             public int compare(String x, String y) {
@@ -74,7 +110,40 @@ public class Main {
         System.out.println(g.poll());
         System.out.print("Tas(int) aprés suppression : ");
         g.affiche();
-
-        System.out.println("Coucou Sven");
+        
+        LinkedList<Integer> LL = new LinkedList<Integer>();
+        ArrayDeque<Integer> AD = new ArrayDeque<Integer>();
+        TBQueue<Integer> TBQ = new TBQueue<Integer>(new Comparator<Integer>() {
+            public int
+                    compare(Integer x, Integer y) {
+                return x.compareTo(y);
+            }
+        }, 1000000);
+        for (int i = 0; i < 1000000; i++) {
+            LL.add(i);
+            AD.add(i);
+            TBQ.add(i);
+        }
+        System.out.println("Temps d'éxecution pour LinkedList<Integer> : ");
+        System.out.print("Carre = ");
+        carree(LL);
+        System.out.print("Addition = ");
+        addition(LL);
+        System.out.print("haveFive = ");
+        haveFive(LL);
+        System.out.println("Temps d'éxecution pour ArrayDeque<Integer> : ");
+        System.out.print("Carre = ");
+        carree(AD);
+        System.out.print("Addition = ");
+        addition(AD);
+        System.out.print("haveFive = ");
+        haveFive(AD);
+        System.out.println("Temps d'éxecution pour TBQueue<Integer> : ");
+        System.out.print("Carre = ");
+        carree(TBQ);
+        System.out.print("Addition = ");
+        addition(TBQ);
+        System.out.print("haveFive = ");
+        haveFive(TBQ);
     }
 }
--- TBQueue.java
+++ TBQueue.java
@@ -1,47 +1,49 @@
+
 import java.util.*;
 import java.util.function.*;
 import java.lang.*;
 import java.lang.Class.*;
 //https://docs.oracle.com/javase/7/docs/api/java/lang/Class.html
-public class TBQueue<E> extends AbstractQueue<E>  implements QueueExt<E>{
+
+public class TBQueue<E extends Object> extends AbstractQueue<E> implements QueueExt<E> {
 
     /*
       Racine indice 0
       fils gauche de l'indice i => 2i+1 et son fils droit => 2i+2
       i > 0 pere gauche => i-1/2
-    */
+     */
     private E[] tas;
     private Comparator<? super E> comp;
     private int courant = 0;
     private int max;
-        
-    public TBQueue(Comparator<? super E> c,int max) {
-		comp = c;
-		// Class<E> aux[];
-		// Object tmp[] = new Object[max];
-		// tas = tmp.getComponentType();	
-		tas = (E[]) new Object[max];
-		System.out.println("ICIII" + tas.getClass().getSimpleName() + "FINI");
-		this.max = max;
+
+    public TBQueue(Comparator<? super E> c, int max) throws ClassCastException {
+        comp = c;
+        // Class<E> aux[];
+        // Object tmp[] = new Object[max];
+        // tas = tmp.getComponentType();
+        tas = (E[]) new Object[max];
+        System.out.println("ICIII" + tas.getClass().getSimpleName() + "FINI");
+        this.max = max;
     }
 
     //@Override
     public Iterator<E> iterator() {
-	return new Iterator<E>() {
-	    private int n = -1;
+        return new Iterator<E>() {
+            private int n = -1;
 
-	    public boolean hasNext() {
-		n++;
-		return (n < courant);
-	    }
+            public boolean hasNext() {
+                n++;
+                return (n < courant);
+            }
 
-	    public E next() {
-		return tas[n];
-	    }
+            public E next() {
+                return tas[n];
+            }
 
-	    public void remove() {
-	    }
-	};
+            public void remove() {
+            }
+        };
     }
 
     /**
@@ -50,7 +52,7 @@ public class TBQueue<E> extends AbstractQueue<E>  implements QueueExt<E>{
      */
     //@Override
     public int size() {
-	return courant;
+        return courant;
     }
 
     /**
@@ -60,20 +62,20 @@ public class TBQueue<E> extends AbstractQueue<E>  implements QueueExt<E>{
      */
     //@Override
     public boolean offer(E e) {
-	if (courant >= max) {
-	    System.out.println("Plus de place: nombre élement = " + courant + "; capacité = " + max);
-	    return false;
-	}
-	tas[courant] =  e;
-	int i = courant;
-	while (i >= 0 && ((i - 1) / 2) >= 0 && tas[(i - 1) / 2] != null && (comp.compare(tas[(i - 1) / 2],tas[i]) < 0)) {
-	    E tmp = tas[(i - 1) / 2];
-	    tas[(i - 1) / 2] = tas[i];
-	    tas[i] = tmp;
-	    i = (i - 1) / 2;
-	}
-	courant++;
-	return true;
+        if (courant >= max) {
+            System.out.println("Plus de place: nombre élement = " + courant + "; capacité = " + max);
+            return false;
+        }
+        tas[courant] = e;
+        int i = courant;
+        while (i >= 0 && ((i - 1) / 2) >= 0 && tas[(i - 1) / 2] != null && (comp.compare(tas[(i - 1) / 2], tas[i]) < 0)) {
+            E tmp = tas[(i - 1) / 2];
+            tas[(i - 1) / 2] = tas[i];
+            tas[i] = tmp;
+            i = (i - 1) / 2;
+        }
+        courant++;
+        return true;
     }
 
     /**
@@ -82,47 +84,47 @@ public class TBQueue<E> extends AbstractQueue<E>  implements QueueExt<E>{
      */
     //@Override
     public E poll() {
-	if (courant <= 0 || max <= 0) {
-	    System.out.println("Pas de racine");
-	    return null;
-	}
-	E res = tas[0];
-	tas[0] = tas[courant - 1];
-	tas[courant - 1] = null;
-	courant--;
-	int i = 0;
-	while (((((2 * i) + 1) < max && tas[(2 * i) + 1] != null)
-		|| (((2 * i) + 2) < max && tas[(2 * i) + 2] != null))){
-	    E tmp = tas[i];
-	    if ((tas[(2 * i) + 1] != null && tas[(2 * i) + 2] == null)) {
-		if (comp.compare(tas[(2 * i) + 1],tas[i]) > 0) {
-		    tas[i] = tas[(2 * i) + 1];
-		    tas[(2 * i) + 1] = tmp;
-		    i = (2 * i) + 1;
-		}else{
-		    break;
-		}
-	    } else if ((tas[(2 * i) + 1] == null && tas[(2 * i) + 2] != null)) {
-		if (comp.compare(tas[(2 * i) + 2],tas[i]) > 0) {
-		    tas[i] = tas[(2 * i) + 2];
-		    tas[(2 * i) + 2] = tmp;
-		    i = (2 * i) + 2;
-		}else{
-		    break;
-		}
-	    } else {
-		if (comp.compare(tas[(2 * i) + 1],tas[(2 * i) + 2]) > 0) {
-		    tas[i] = tas[(2 * i) + 1];
-		    tas[(2 * i) + 1] = tmp;
-		    i = (2 * i) + 1;
-		} else {
-		    tas[i] = tas[(2 * i) + 2];
-		    tas[(2 * i) + 2] = tmp;
-		    i = (2 * i) + 2;
-		}
-	    }
-	}
-	return res;
+        if (courant <= 0 || max <= 0) {
+            System.out.println("Pas de racine");
+            return null;
+        }
+        E res = tas[0];
+        tas[0] = tas[courant - 1];
+        tas[courant - 1] = null;
+        courant--;
+        int i = 0;
+        while (((((2 * i) + 1) < max && tas[(2 * i) + 1] != null)
+                || (((2 * i) + 2) < max && tas[(2 * i) + 2] != null))) {
+            E tmp = tas[i];
+            if ((tas[(2 * i) + 1] != null && tas[(2 * i) + 2] == null)) {
+                if (comp.compare(tas[(2 * i) + 1], tas[i]) > 0) {
+                    tas[i] = tas[(2 * i) + 1];
+                    tas[(2 * i) + 1] = tmp;
+                    i = (2 * i) + 1;
+                } else {
+                    break;
+                }
+            } else if ((tas[(2 * i) + 1] == null && tas[(2 * i) + 2] != null)) {
+                if (comp.compare(tas[(2 * i) + 2], tas[i]) > 0) {
+                    tas[i] = tas[(2 * i) + 2];
+                    tas[(2 * i) + 2] = tmp;
+                    i = (2 * i) + 2;
+                } else {
+                    break;
+                }
+            } else {
+                if (comp.compare(tas[(2 * i) + 1], tas[(2 * i) + 2]) > 0) {
+                    tas[i] = tas[(2 * i) + 1];
+                    tas[(2 * i) + 1] = tmp;
+                    i = (2 * i) + 1;
+                } else {
+                    tas[i] = tas[(2 * i) + 2];
+                    tas[(2 * i) + 2] = tmp;
+                    i = (2 * i) + 2;
+                }
+            }
+        }
+        return res;
     }
 
     /**
@@ -131,83 +133,91 @@ public class TBQueue<E> extends AbstractQueue<E>  implements QueueExt<E>{
      */
     //@Override
     public E peek() {
-	return tas[0];
+        return tas[0];
     }
 
     /**
-     * @return une nouvelle liste consistant en les éléments de tab qui satisfont le prédicat cond.
+     * @return une nouvelle liste consistant en les éléments de tab qui
+     * satisfont le prédicat cond.
      */
     //@Override
-    public QueueExt<E> filtre(Predicate<E> cond){
-	ArrayList<E> tmp = new ArrayList<E>();
-	for(E x:this){
-	    if(cond.test(x)) tmp.add(x);
-	}
-	QueueExt<E> res = new TBQueue<E>(comp,tmp.size());
-        
-	for(E x:tmp){
-	    res.offer(x);
-	}
-	return res;
+    public QueueExt<E> filtre(Predicate<E> cond) {
+        ArrayList<E> tmp = new ArrayList<E>();
+        for (E x : this) {
+            if (cond.test(x)) {
+                tmp.add(x);
+            }
+        }
+        QueueExt<E> res = new TBQueue<E>(comp, tmp.size());
+
+        for (E x : tmp) {
+            res.offer(x);
+        }
+        return res;
     }
-    
+
     /**
-     * @return une liste dont les éléments sont tous les éléments de tab auxquels on a appliqué la fonction f.
+     * @return une liste dont les éléments sont tous les éléments de tab
+     * auxquels on a appliqué la fonction f.
      */
     //@Override
-    public <U> QueueExt<U> map(Function<E, U> f){
-        QueueExt<U> res = new TBQueue<U>(new Comparator<U>(){
-            public int compare(U x, U y){
-               	// E tmp;
+    public <U> QueueExt<U> map(Function<E, U> f) {
+        QueueExt<U> res = new TBQueue<U>(new Comparator<U>() {
+            public int compare(U x, U y) {
+                // E tmp;
                 // if ((tmp).isAssignableFrom(x.getClass()) && (tmp).isAssignableFrom(y.getClass()) ) {
-                	return comp.compare((E)x,(E)y);
+                return comp.compare((E) x, (E) y);
                 // }else{
-                	// System.out.println("Cast pas possible");
-                	// return 0;
+                // System.out.println("Cast pas possible");
+                // return 0;
 
                 // }
             }
-        },max);
-        for(E x: this){
+        }, max);
+        for (E x : this) {
             res.offer(f.apply(x));
         }
-	return res;
-    }
-    
-    /**
-     * @return un optionnel contenant un élément de la liste satisfaisant le prédicat cond, s’il en existe, sinon l’optionnel vide.
-     */
-    //@Override
-    public Optional<E> trouve(Predicate<E> cond){
-        for (E x:this) {
-            if(cond.test(x)) return Optional.of(x);
-        }
-	return Optional.empty();
+        return res;
     }
 
     /**
-     * Initialise un accumulateur a avec z, puis, pour chaque élément x de this, calcule a = f (a, x).
+     * @return un optionnel contenant un élément de la liste satisfaisant le
+     * prédicat cond, s’il en existe, sinon l’optionnel vide.
+     */
+    //@Override
+    public Optional<E> trouve(Predicate<E> cond) {
+        for (E x : this) {
+            if (cond.test(x)) {
+                return Optional.of(x);
+            }
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Initialise un accumulateur a avec z, puis, pour chaque élément x de this,
+     * calcule a = f (a, x).
+     *
      * @return a
      */
-
     //@Override
-    public <U> U reduit(U z, BiFunction<U, E, U> f){
+    public <U> U reduit(U z, BiFunction<U, E, U> f) {
         U acc = z;
-        for (E x:this) {
+        for (E x : this) {
             acc = f.apply(acc, x);
         }
-	return acc;
+        return acc;
     }
 
     public void affiche() {
-	if(courant <= 0 || max <= 0){
-	    System.out.println("Pas d'élément a afficher");
-	    return;
-	}
-	System.out.print("[");
-	for (int i = 0; i < courant - 1; i++) {
-	    System.out.print("\""+tas[i]+"\"" + " , ");
-	}
-	System.out.println("\""+tas[courant - 1]+"\"" + "]");
+        if (courant <= 0 || max <= 0) {
+            System.out.println("Pas d'élément a afficher");
+            return;
+        }
+        System.out.print("[");
+        for (int i = 0; i < courant - 1; i++) {
+            System.out.print("\"" + tas[i] + "\"" + " , ");
+        }
+        System.out.println("\"" + tas[courant - 1] + "\"" + "]");
     }
 }
